a87ba7938e754d65b0e8f75557a95dd4bd994956
aled <aled@users.sourceforge.net>
2010-09-15 22:22:31 +0100

    Add oauth credentials for api06.dev.openstreetmap.org

--- src/main/java/com/wibblr/arriate/auth/ParameterParser.java
+++ src/main/java/com/wibblr/arriate/auth/ParameterParser.java
@@ -1,11 +1,5 @@
 package com.wibblr.arriate.auth;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-
-import org.apache.commons.codec.DecoderException;
 
 public class ParameterParser {
 	
